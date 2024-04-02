1c8ed502ca426cdfacdd5dd1265451b92ba88452
Din0zaBr <ivan.zabolotnyi2001@mail.ru>
2024-04-02 11:44:47 +0300

    1. Количество потоков в ThreadPoolExecutor настроено на количество ядер процессора, что позволяет максимально эффективно использовать ресурсы системы. 2. Использование os.cpu_count() для определения количества ядер процессора улучшает производительность, позволяя оптимизировать параллельную обработку. 3. Столбец publishedDate преобразуется в тип данных category, что может уменьшить использование памяти и ускорить некоторые операции. 4. Использование astype('category') для преобразования дат в категории может улучшить производительность, особенно при работе с большими объемами данных.

--- CountOfCVE/CVE.py
+++ CountOfCVE/CVE.py
@@ -1,46 +1,45 @@
-import pandas as pd
 import glob
-from concurrent.futures import ThreadPoolExecutor
-import orjson
-import zipfile
 import os
+import zipfile
+from concurrent.futures import ThreadPoolExecutor
+
+import orjson
+import pandas as pd
 
 # Установка настроек отображения для полного вывода данных
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
 
-def unzip_files(zip_file_path):
+def unzip_and_read_file(zip_file_path):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(os.path.dirname(zip_file_path))
+        for file in zip_ref.namelist():
+            if file.endswith('.json'):
+                with zip_ref.open(file) as f:
+                    return orjson.loads(f.read())['CVE_Items']
 
 
-def read_file(filename):
-    with open(filename, 'rb') as f:
-        return orjson.loads(f.read())['CVE_Items']
+# Определение количества ядер процессора для оптимизации параллельной обработки
+num_cores = os.cpu_count() or 4
 
-
-# Разархивация всех .zip файлов в текущем каталоге
-for zip_file in glob.glob('*.zip'):
-    unzip_files(zip_file)
-
-# Чтение и объединение данных из всех файлов с использованием многопоточности
-with ThreadPoolExecutor() as ex:
-    merged_data = [item for lst in ex.map(read_file, glob.glob('*.json')) for item in lst]
+# Разархивация всех .zip файлов в текущем каталоге и чтение данных
+with ThreadPoolExecutor(max_workers=num_cores) as ex:
+    merged_data = [item for lst in ex.map(unzip_and_read_file, glob.glob('*.zip')) for item in lst]
 
 # Преобразование списка в DataFrame
-data = pd.json_normalize(merged_data)
-
+data = pd.DataFrame(merged_data)
 
 # Обработка и анализ данных
-data['publishedDate'] = pd.to_datetime(data['publishedDate'])
-cve_per_day = data.groupby(data['publishedDate'].dt.date).size()
+# Использование 'category' для столбца 'publishedDate' для уменьшения использования памяти
+data['publishedDate'] = pd.to_datetime(data['publishedDate']).dt.date.astype('category')
+cve_per_day = data.groupby('publishedDate').size()
 
 # Вывод результата
 print("Number of CVEs per Day:")
 for date, count in cve_per_day.items():
     # print(f"{date}: {count}")
-    print(f"{count}", end='  ') # для будущей печати
+    print(f"{count}", end=' ')  # для будущей печати
+    print()
 
 summary_stats = cve_per_day.agg(['min', 'max', 'mean'])
 date_with_max_cve = cve_per_day.idxmax()

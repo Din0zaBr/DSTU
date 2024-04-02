ba95efe1db887beb47c4ffd026fdd9a380027fa2
DinozaBr <ivan.zabolotnyi2001@mail.ru>
2024-04-02 11:27:13 +0300

    Добавил строку для удобного вывода (43)
    Добавил минимальное, максимально, среднее значения

--- CountOfCVE/CVE.py
+++ CountOfCVE/CVE.py
@@ -9,13 +9,16 @@ import os
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
+
 def unzip_files(zip_file_path):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(os.path.dirname(zip_file_path))
 
+
 def read_file(filename):
     with open(filename, 'rb') as f:
         return orjson.loads(f.read())['CVE_Items']
+
 
 # Разархивация всех .zip файлов в текущем каталоге
 for zip_file in glob.glob('*.zip'):
@@ -28,12 +31,16 @@ with ThreadPoolExecutor() as ex:
 # Преобразование списка в DataFrame
 data = pd.json_normalize(merged_data)
 
+
 # Обработка и анализ данных
 data['publishedDate'] = pd.to_datetime(data['publishedDate'])
 cve_per_day = data.groupby(data['publishedDate'].dt.date).size()
 
 # Вывод результата
-print(cve_per_day)
+print("Number of CVEs per Day:")
+for date, count in cve_per_day.items():
+    # print(f"{date}: {count}")
+    print(f"{count}", end='  ') # для будущей печати
 
 summary_stats = cve_per_day.agg(['min', 'max', 'mean'])
 date_with_max_cve = cve_per_day.idxmax()

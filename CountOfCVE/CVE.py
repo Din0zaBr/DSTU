94912f2a5bb0fcf7762086e1760ab409893af139
DinozaBr <ivan.zabolotnyi2001@mail.ru>
2024-04-18 20:02:35 +0300

    Поехали

--- CountOfCVE/CVE.py
+++ CountOfCVE/CVE.py
@@ -1,13 +1,21 @@
+import os
+
+
 def count_seconds_from_file(file_path):
-    total_seconds = 0
+    if not os.path.exists(file_path):
+        print(f"Файл {file_path} не найден.")
+        return []
+
+    seconds_list = []
     with open(file_path, 'r') as file:
         for line in file:
             minutes, seconds = map(int, line.strip().split(':'))
-            total_seconds += minutes * 60 + seconds
-    return total_seconds
+            total_seconds = minutes * 60 + seconds
+            seconds_list.append(total_seconds)
+    return seconds_list
 
 
 # Замените 'times.txt' на путь к вашему файлу, если он отличается
 file_path = 'times.txt'
-total_seconds = count_seconds_from_file(file_path)
-print(f"Общее количество секунд: {total_seconds}")
+seconds_list = count_seconds_from_file(file_path)
+print(f"Список секунд: {seconds_list}")

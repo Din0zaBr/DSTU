0613fcd5a080289d3af4d6ae0328a6f364c6c137
DinozaBr <ivan.zabolotnyi2001@mail.ru>
2024-04-18 20:15:50 +0300

    Поехали заполнять

--- CountOfCVE/CVE.py
+++ CountOfCVE/CVE.py
@@ -2,20 +2,33 @@ import os
 
 
 def count_seconds_from_file(file_path):
+    """
+    Считает общее количество секунд, указанных в файле.
+
+    Параметры:
+    file_path (str): Путь к файлу, содержащему временные отметки в формате 'минуты:секунды'.
+
+    Возвращает:
+    int: Общее количество секунд, указанных в файле.
+
+    Исключения:
+    FileNotFoundError: Если файл не найден.
+    """
+    # Проверяем, существует ли файл, и если нет, создаем его
     if not os.path.exists(file_path):
-        print(f"Файл {file_path} не найден.")
+        with open(file_path, 'w') as file:
+            pass  # Файл создается, но не выводится никакой информации
+        print(f"Наполните файл times.txt данными")
         return []
 
-    seconds_list = []
     with open(file_path, 'r') as file:
-        for line in file:
-            minutes, seconds = map(int, line.strip().split(':'))
-            total_seconds = minutes * 60 + seconds
-            seconds_list.append(total_seconds)
-    return seconds_list
+        times_list = [minutes * 60 + seconds for line in file for minutes, seconds in
+                      [map(int, line.strip().split(':'))]]
+    return times_list
 
 
-# Замените 'times.txt' на путь к вашему файлу, если он отличается
-file_path = 'times.txt'
-seconds_list = count_seconds_from_file(file_path)
-print(f"Список секунд: {seconds_list}")
+if __name__ == "__main__":
+    # Автоматически узнаем адрес файла times.txt
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(current_directory, 'times.txt')
+    Data = count_seconds_from_file(file_path)

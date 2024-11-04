1a8ea3063dd96cf0f86f6b998515feadda850490
DinozaBr <ivan.zabolotnyi2001@mail.ru>
2024-11-05 00:03:54 +0300

    Рабочее кодирование LSW

--- python/TheoryOfInformation/1lab/utils_for_encode_decode.py
+++ python/TheoryOfInformation/1lab/utils_for_encode_decode.py
@@ -46,7 +46,8 @@ def huffman_encode(file_path):
         file.write(encoded_text)
 
     print('Huffman coding table:')
-    for char, code in huffman_table.items():
+    sorted_huffman_table = sorted(huffman_table.items(), key=lambda x: x[1])
+    for char, code in sorted_huffman_table:
         print(f'{char}: {code}')
 
 
@@ -88,12 +89,94 @@ def lz77_encode(file_path):
         return
     if not confirm_operation('закодировать'):
         return
-    pass
 
+    with open(file_path, 'r') as file:
+        data = file.read()
+    # window_size = 12
+    # while i < len(data):
+    #     match_length = 0
+    #     match_distance = 0
+    #     next_char = data[i]
+    #
+    #     # Define the search window
+    #     start_window = max(0, i - window_size)
+    #     search_buffer = data[start_window:i]
+    #
+    #     # Try to find the longest match
+    #     for j in range(len(search_buffer)):
+    #         length = 0
+    #         while (length < len(data) - i and
+    #                search_buffer[j:j + length + 1] == data[i:i + length + 1]):
+    #             length += 1
+    #
+    #         # Update the best match if found
+    #         if length >= match_length:
+    #             match_length = length
+    #             match_distance = len(search_buffer) - j
+    #             if i + match_length < len(data):
+    #                 next_char = data[i + match_length]
+    #             else:
+    #                 next_char = ''
+    #
+    #             # Append (distance, length, next character) tuple
+    #     if match_length > 0:
+    #         compressed_data.append((match_distance, match_length, next_char))
+    #         i += match_length + 1
+    #     else:
+    #         compressed_data.append((0, 0, next_char))
+    #         i += 1
+    # print(compressed_data)
+    pass
 
 def lzw_encode(file_path):
     if not check_file_existence(file_path):
         return
     if not confirm_operation('закодировать'):
         return
-    pass
+
+    with open(file_path, 'r') as file:
+        data = file.read()
+
+    data = data.lower()  # Приведение к нижнему регистру
+    sp_zn = []
+
+    # Сбор уникальных символов
+    for i in data:
+        if i not in sp_zn:
+            sp_zn.append(i)
+
+    # Создание словаря для кодирования
+    sl = {x: sp_zn.index(x) for x in sp_zn}
+
+    num = len(sl)
+    current = data[0]
+    cur_str = ''
+    it_str = ''
+    cur_str = current
+
+    # Основная логика алгоритма LZW
+    for i in data[1:]:
+        next = i
+        if (cur_str + next) not in sl:
+            sl[cur_str + next] = num
+            num += 1
+            it_str += str(sl[cur_str]) + ' '
+            current = next
+            cur_str = current
+        else:
+            current = next
+            cur_str += next
+
+    sl[cur_str + "@"] = num
+    it_str += str(sl[cur_str])
+
+    # Формирование результата
+    encoded_text = str(sl) + '\n' + it_str.strip()
+    encoded_file_path = os.path.join(os.path.dirname(file_path),
+                                     os.path.splitext(os.path.basename(file_path))[0] + '_LWS_encoded' +
+                                     os.path.splitext(os.path.basename(file_path))[-1])
+    with open(encoded_file_path, 'w') as file:
+        file.write(encoded_text)
+
+    print(encoded_text)
+

b122c27c8e1af4470e1007992342fbb6064e36a7
Din0zaBr <ivan.zabolotnyi2001@mail.ru>
2024-11-18 21:17:37 +0300

    реализовали преобразование элементов текстовой строки в бинарный вид с незначащими нулями

--- python/TheoryOfInformation/1lab/test.py
+++ python/TheoryOfInformation/1lab/test.py
@@ -1,7 +1,18 @@
-txt, ascii_txt = input(), ''
+txt, bin_list_txt, max_len_el = input(), [], -1
 for el in txt:
-    ascii_txt += str(bin(ord(el)))[2:]
-    print(ascii_txt)
+    temp = str(bin(ord(el)))[2:]
+    len_temp = len(temp)
+    print(el, end=" ")
+
+    bin_list_txt.append(temp)
+
+    if len_temp > max_len_el:
+        max_len_el = len_temp
+print()
+print(bin_list_txt)
+
+bin_list_txt = ['0' * (max_len_el - len(el)) + el for el in bin_list_txt]
+print(bin_list_txt)
 
 
 def input_matrix(k, n):
@@ -17,21 +28,27 @@ def input_matrix(k, n):
     print(f"Введите элементы матрицы {k}x{n} (каждый элемент должен быть либо 1, либо 0):")
 
     for i in range(k):
-        row = []
-        print(f"Введите элементы {i + 1}-й строки (через пробел):")
-        row_input = input().split()
+        while True:
+            row = []
+            print(f"Введите элементы {i + 1}-й строки (через пробел):")
+            row_input = input().split()
 
-        if len(row_input) != n:
-            print(f"Ошибка: Введено неверное количество элементов. Ожидалось {n}, получено {len(row_input)}.")
-            return None
+            if len(row_input) != n:
+                print(
+                    f"Ошибка: Введено неверное количество элементов. Ожидалось {n}, получено {len(row_input)}. Попробуйте снова.")
+                continue
 
-        for elem in row_input:
-            if elem not in ['0', '1']:
-                print(f"Ошибка: '{elem}' не является допустимым значением (должно быть 0 или 1).")
-                return None
-            row.append(int(elem))
+            valid = True
+            for elem in row_input:
+                if elem not in ['0', '1']:
+                    print(f"Ошибка: '{elem}' не является допустимым значением (должно быть 0 или 1). Попробуйте снова.")
+                    valid = False
+                    break
+                row.append(int(elem))
 
-        matrix.append(row)
+            if valid:
+                matrix.append(row)
+                break
 
     return matrix
 
@@ -42,9 +59,6 @@ n = int(input("Введите количество столбцов (n): "))
 
 matrix = input_matrix(k, n)
 
-if matrix:
-    print("Введенная матрица:")
-    for row in matrix:
-        print(row)
-else:
-    print("Ошибка при вводе матрицы.")
+print("Введенная матрица:")
+for row in matrix:
+    print(row)

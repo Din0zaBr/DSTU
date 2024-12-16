1922bd009ca7f182bb63047c8f8ba2399f3f58ca
Din0zaBr <ivan.zabolotnyi2001@mail.ru>
2024-12-16 23:20:31 +0300

    Убрал лишнюю функцию по удалению строки + столбца

--- python/TheoryOfInformation/1lab/utils_for_matrix_representation_of_block_codes.py
+++ python/TheoryOfInformation/1lab/utils_for_matrix_representation_of_block_codes.py
@@ -514,17 +514,6 @@ def shorten_matrix(matrix, rows, cols):
     return matrix
 
 
-def puncture_matrix(matrix, rows, cols):
-    """
-    Перфорирует матрицу, удаляя строки и столбцы с заданными индексами.
-    """
-    for row in sorted(rows, reverse=True):
-        matrix = [matrix[i] for i in range(len(matrix)) if i != row]
-    for col in sorted(cols, reverse=True):
-        matrix = [[matrix[i][j] for j in range(len(matrix[i])) if j != col] for i in range(len(matrix))]
-    return matrix
-
-
 def extend_matrix(matrix):
     """
     Расширяет матрицу, добавляя строку сверху и после столбец слева.
@@ -585,7 +574,7 @@ def modify_matrix_window():
                 elif len(rows) == len(Hsys) or len(cols) == len(Hsys[0]):
                     popup_error('Матрица не может содержать ни строк, ни столбцов после удаления.')
                 else:
-                    modified_Hsys = puncture_matrix(Hsys.copy(), rows, cols)
+                    modified_Hsys = shorten_matrix(Hsys.copy(), rows, cols)
                     n = len(modified_Hsys[0])
                     k = len(modified_Hsys)
                     modified_Hsys_to_Gsys = get_system_new(modified_Hsys, n, k)

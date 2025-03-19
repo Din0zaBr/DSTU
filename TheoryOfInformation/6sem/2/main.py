83f4062be39a80be7a4023636fb3e8d414bc840d
DinozaBr <ivan.zabolotnyi2001@mail.ru>
2025-03-19 10:19:15 +0300

    Не удержался и написал типы

--- TheoryOfInformation/6sem/2/main.py
+++ TheoryOfInformation/6sem/2/main.py
@@ -1,78 +1,79 @@
 import PySimpleGUI as sg
+from typing import List, Tuple
 
 
-def text_to_binary(text):
+def text_to_binary(text: str) -> str:
     """Переводит текст в двоичную строку (ASCII)"""
     return ''.join(f"{ord(c):08b}" for c in text)
 
 
-def binary_to_text(binary_str):
+def binary_to_text(binary_string: str) -> str:
     """Переводит двоичную строку обратно в текст (ASCII)"""
-    chars = []
-    for i in range(0, len(binary_str), 8):
-        byte = binary_str[i:i + 8]
+    chars: list = []
+    for i in range(0, len(binary_string), 8):
+        byte: str = binary_string[i:i + 8]
         chars.append(chr(int(byte, 2)))
     return ''.join(chars)
 
 
-def convolutional_encode(input_bits, polynomials) -> str:
-    """Сверточное кодирование"""
+def convolutional_encode(input_bits: str, polynomials: List[Tuple[int, ...]]) -> str:
+    """Свёрточное кодирование"""
     if not input_bits:
         return ''
 
-    max_register = max(max(tup) for tup in polynomials)
-    shift_register = [0] * (max_register + 1)
-    encoded_data = []
+    max_register: int = max(max(tup) for tup in polynomials)
+    shift_register: list = [0] * (max_register + 1)
+    encoded_data: list = []
 
     for bit in input_bits:
         shift_register.insert(0, int(bit))
         shift_register.pop()
 
         for tup in polynomials:
-            xor = sum(shift_register[index] for index in tup) % 2
+            xor: int = sum(shift_register[index] for index in tup) % 2
             encoded_data.append(str(xor))
 
     return ''.join(encoded_data)
 
 
-def viterbi_decode(encoded_bits, polynomials) -> str:
+def viterbi_decode(encoded_bits: str, polynomials: List[Tuple[int, ...]]) -> str:
     """Алгоритм Витерби"""
     if not encoded_bits:
         return ''
 
-    bit_outputs = len(polynomials)
-    max_register = max(max(tup) for tup in polynomials)
-    count_bit_states = 2 ** max_register
-    states = [format(i, f'0{max_register}b') for i in range(count_bit_states)]
+    bit_outputs: int = len(polynomials)
+    max_register: int = max(max(tup) for tup in polynomials)
+    count_bit_states: int = 2 ** max_register
+    states: list = [format(i, f'0{max_register}b') for i in range(count_bit_states)]
 
-    path_metrics = {s: float('inf') for s in states}
+    path_metrics: dict = {s: float('inf') for s in states}
     path_metrics['0' * max_register] = 0
-    paths = {s: [] for s in states}
+    paths: dict = {s: [] for s in states}
 
     for step in range(0, len(encoded_bits) // bit_outputs):
-        current_bits = encoded_bits[step * bit_outputs: (step + 1) * bit_outputs]
-        new_metrics = {s: float('inf') for s in states}
-        new_paths = {s: [] for s in states}
+        current_bits: str = encoded_bits[step * bit_outputs: (step + 1) * bit_outputs]
+        new_metrics: dict = {s: float('inf') for s in states}
+        new_paths: dict = {s: [] for s in states}
 
         for state in states:
             if path_metrics[state] == float('inf'):
                 continue
             for input_bit in ['0', '1']:
-                next_state = (input_bit + state)[:-1]
-                switch = list(map(int, input_bit + state))
+                next_state: str = (input_bit + state)[:-1]
+                shift_register: list = list(map(int, input_bit + state))
 
-                expected_bits_list = []
+                expected_bits_list: list = []
                 for tup in polynomials:
-                    xor = sum(switch[index] for index in tup) % 2
+                    xor: int = sum(shift_register[index] for index in tup) % 2
                     expected_bits_list.append(str(xor))
                 expected_str = ''.join(expected_bits_list)
 
-                hammings_weight = sum(
+                hammings_weight: int = sum(
                     1
                     for bit_in_current_bits, bit_in_expected_str in zip(current_bits, expected_str)
                     if bit_in_current_bits != bit_in_expected_str
                 )
-                total_hammings_weight = path_metrics[state] + hammings_weight
+                total_hammings_weight: int = path_metrics[state] + hammings_weight
 
                 if total_hammings_weight < new_metrics[next_state]:
                     new_metrics[next_state] = total_hammings_weight
@@ -80,9 +81,9 @@ def viterbi_decode(encoded_bits, polynomials) -> str:
 
         path_metrics, paths = new_metrics, new_paths
 
-    final_state = min(path_metrics, key=path_metrics.get)
+    final_state: int = min(path_metrics, key=path_metrics.get)
     result = ''.join(paths[final_state])
-    return result[:len(encoded_bits) // len(polynomials)]
+    return result
 
 
 def create_layout():
@@ -108,9 +109,9 @@ def main():
             break
         if event == 'Кодировать':
             try:
-                summators_input = values['summators'].strip().split('\n')
+                summators_input: str = values['summators'].strip().split('\n')
 
-                summators = [tuple(map(int, line.split(','))) for line in summators_input]
+                summators: list = [tuple(map(int, line.split(','))) for line in summators_input]
 
                 # Проверка на уникальность сумматоров (независимо от порядка)
                 # unique_summators = set(frozenset(s) for s in summators)
@@ -118,23 +119,23 @@ def main():
                 #     sg.popup_error('Сумматоры должны быть уникальными (независимо от порядка регистров).')
                 #     continue
 
-                sequence = values['sequence']
+                sequence: str = values['sequence']
 
                 if all(c in '01' for c in sequence):
-                    binary_data = sequence
+                    binary_data: str = sequence
                 else:
-                    binary_data = text_to_binary(sequence)
+                    binary_data: str = text_to_binary(sequence)
 
-                encoded_data = convolutional_encode(binary_data, summators)
+                encoded_data: str = convolutional_encode(binary_data, summators)
                 print(f"Закодированные данные: {encoded_data}")
             except Exception as e:
                 sg.popup_error(f'Ошибка при кодировании: {e}')
 
         if event == 'Декодировать':
             try:
-                summators_input = values['summators'].strip().split('\n')
+                summators_input: str = values['summators'].strip().split('\n')
 
-                summators = [tuple(map(int, line.split(','))) for line in summators_input]
+                summators: list = [tuple(map(int, line.split(','))) for line in summators_input]
 
                 # Проверка на уникальность сумматоров (независимо от порядка)
                 # unique_summators = set(frozenset(s) for s in summators)
@@ -142,10 +143,10 @@ def main():
                 #     sg.popup_error('Сумматоры должны быть уникальными (независимо от порядка регистров).')
                 #     continue
 
-                encoded_sequence = values['sequence']
+                encoded_sequence: str = values['sequence']
 
-                decoded_data = viterbi_decode(encoded_sequence, summators)
-                decoded_text = binary_to_text(decoded_data)
+                decoded_data: str = viterbi_decode(encoded_sequence, summators)
+                decoded_text: str = binary_to_text(decoded_data)
                 print(f"Декодированные данные: {decoded_text}")
             except Exception as e:
                 sg.popup_error(f'Ошибка при декодировании: {e}')
